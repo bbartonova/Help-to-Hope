168a5cee148132938527833283fa47ae2d2a0c13
bbartonova <72999251+bbartonova@users.noreply.github.com>
2021-05-30 20:22:13 +0200

    Jedeme styly (#12)

--- components/header/Header.js
+++ components/header/Header.js
@@ -4,18 +4,18 @@ import styles from '../../styles/Header.module.scss';
 
 export default function Header(props) {
   return (
-    <header className={styles.header}>
-      <ul>
+    <header className="font-title bg-grey-dark">
+      <ul className="m-0">
         {!props.isLandingPage && (
-          <li>
+          <li className="w-1/3">
             <Link href="/">
               <a>úvod</a>
             </Link>
           </li>
         )}
-        <li>
+        <li className="flex justify-center items-center">
           <Link href="/">
-            <a>
+            <a className="m-0">
               <Image
                 src="/HtH_logo_rev_sm.png"
                 alt="logo"
@@ -28,8 +28,8 @@ export default function Header(props) {
         </li>
         {!props.isLandingPage && (
           <li>
-            <Link href="/">
-              <a>facebook</a>
+            <Link href="https://www.facebook.com/HelptoHopeCZ/">
+              <a>Facebook</a>
             </Link>
           </li>
         )}
